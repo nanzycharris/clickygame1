0e29b8042e2dcfeea1457b610596eeb6efb22b39
Nancy Harris <nanzyharris@gmail.com>
2020-02-20 20:14:36 -0600

    Updates

---
+++ precache-manifest.e34bda07ad5ab8c61ccda8ec99030bd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfc072edc943b9f1e02ddbd68de40d9",
+    "url": "/clickygame1/index.html"
+  },
+  {
+    "revision": "1282290af5934244e1a8",
+    "url": "/clickygame1/static/css/main.cccf6242.chunk.css"
+  },
+  {
+    "revision": "338b3cc11946a46de1d7",
+    "url": "/clickygame1/static/js/2.cc05411a.chunk.js"
+  },
+  {
+    "revision": "86b4fce93093fa1e24b23e8f37d5f666",
+    "url": "/clickygame1/static/js/2.cc05411a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1282290af5934244e1a8",
+    "url": "/clickygame1/static/js/main.d69ee7a4.chunk.js"
+  },
+  {
+    "revision": "b19b73784584ace0d7ca",
+    "url": "/clickygame1/static/js/runtime-main.dc6f7085.js"
+  }
+]);
